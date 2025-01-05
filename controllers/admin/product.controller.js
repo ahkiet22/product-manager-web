8152d08cd5b049ed2a7e9272183640fdf7031680
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-01-05 16:39:51 +0700

    Status filtering feature

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,14 +1,48 @@
-const Product = require("../../models/product.model")
+const Product = require("../../models/product.model");
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
-    deleted: false
-  })
+  let filterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
+  // console.log(req.query.status);
+  if (req.query.status) {
+    const index = filterStatus.findIndex(item => item.status == req.query.status);
+    filterStatus[index].class = "active"
+  } else {
+    const index = filterStatus.findIndex(item => item.status == "");
+    filterStatus[index].class = "active"
+  }
+  
+  let find = {
+    deleted: false,
+  };
 
-  console.log(products)
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  const products = await Product.find(find);
+
+  // console.log(products)
 
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Danh sách sản phẩm",
-    products: products
+    products: products,
+    filterStatus: filterStatus,
   });
 };
