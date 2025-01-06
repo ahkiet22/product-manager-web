a99bb41fb36f75d0f7838efa0fd4d6f0aeaad50e
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-01-07 02:50:18 +0700

    optimize feature pagination ( Supports reuse code)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,6 +2,7 @@ const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -23,20 +24,25 @@ module.exports.index = async (req, res) => {
   }
 
   // Pagination
-  let objectPagination = {
-    currentPage: 1,
-    limitItem: 4,
-  };
-  if (req.query.page) {
-    objectPagination.currentPage = parseInt(req.query.page);
-  }
-  objectPagination.skip =
-    (objectPagination.currentPage - 1) * objectPagination.limitItem;
-
   const countProducts = await Product.countDocuments(find);
-  const totalPage = Math.ceil(countProducts / objectPagination.limitItem);
-  objectPagination.totalPage = totalPage;
-  // console.log(countProducts);
+
+  let objectPagination = paginationHelper(
+    {
+      currentPage: 1,
+      limitItem: 4,
+    },
+    req.query,
+    countProducts
+  );
+  // if (req.query.page) {
+  //   objectPagination.currentPage = parseInt(req.query.page);
+  // }
+  // objectPagination.skip =
+  //   (objectPagination.currentPage - 1) * objectPagination.limitItem;
+
+  // const countProducts = await Product.countDocuments(find);
+  // const totalPage = Math.ceil(countProducts / objectPagination.limitItem);
+  // objectPagination.totalPage = totalPage;
   // End Pagination
 
   const products = await Product.find(find)
