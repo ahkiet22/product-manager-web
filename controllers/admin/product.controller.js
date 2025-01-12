7771746ea3447c9316970b14980cfda6cde452d6
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-01-13 00:34:05 +0700

    feat: Edit product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -151,7 +151,6 @@ module.exports.create = async (req, res) => {
 
 // [POST] /admin/products/create
 module.exports.createPost = async (req, res) => {
-  
   req.body.price = parseInt(req.body.price);
   req.body.discountPercentage = parseInt(req.body.discountPercentage);
   req.body.stock = parseInt(req.body.stock);
@@ -171,4 +170,45 @@ module.exports.createPost = async (req, res) => {
   await product.save();
 
   res.redirect(`${systemConfig.prefixAdmin}/products`);
+};
+
+// [GET] /admin/products/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const find = {
+      deleted: false,
+      _id: req.params.id,
+    };
+    const product = await Product.findOne(find);
+    // console.log(product);
+    res.render("admin/pages/products/edit.pug", {
+      pageTitle: "Chỉnh sửa sản phẩm",
+      product: product,
+    });
+  } catch (error) {
+    req.flash("error", "Không tồn tại sản phẩm này!");
+    res.redirect(`${systemConfig.prefixAdmin}/products`);
+  }
+};
+
+// [PATCH] /admin/products/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+  req.body.price = parseInt(req.body.price);
+  req.body.discountPercentage = parseInt(req.body.discountPercentage);
+  req.body.stock = parseInt(req.body.stock);
+  req.body.position = parseInt(req.body.position);
+  console.log(req.file);
+  if (req.file) {
+    req.body.thumbnail = `/uploads/${req.file.filename}`;
+  }
+
+  try {
+    await Product.updateOne({ _id: id }, req.body);
+    req.flash("success", "Cập nhập sản phẩm thành công!")
+  } catch (error) {
+    req.flash("error", "Cập nhập sản phẩm thất bại!")
+  }
+
+  res.redirect("back");
 };
