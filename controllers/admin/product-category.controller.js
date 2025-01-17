83f0cd78086baf3d9e66eede5d86aa14fa40a616
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-01-18 01:58:15 +0700

    feat: side product category feature: + create category + display category -- also added --

---
+++ controllers/admin/product-category.controller.js
@@ -0,0 +1,38 @@
+const ProductCategory = require("../../models/product-category.model");
+const systemConfig = require("../../config/system");
+
+// [GET] /admin/products-category
+module.exports.index = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+  const record = await ProductCategory.find(find);
+
+  res.render("admin/pages/products-category/index.pug", {
+    pageTitle: "Danh mục sản phẩm",
+    record: record,
+  });
+};
+
+// [GET] /admin/products-category/create
+module.exports.create = async (req, res) => {
+  res.render("admin/pages/products-category/create.pug", {
+    pageTitle: "Tạo danh mục sản phẩm",
+  });
+};
+
+// [POST] /admin/products-category/create
+module.exports.createPost = async (req, res) => {
+  console.log(req.body);
+  if (req.body.position == "") {
+    const count = await ProductCategory.countDocuments();
+    req.body.position = count + 1;
+  } else {
+    req.body.position = parseInt(req.body.position);
+  }
+
+  const record = new ProductCategory(req.body);
+  await record.save();
+
+  res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+};
