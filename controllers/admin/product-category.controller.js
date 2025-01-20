c8a2aaee6fda3595407241ed71a6969f11f1ecac
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-01-20 13:57:37 +0700

    feat: implement parent-child structure for products-category (e.g: phone>samsung)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,23 +1,38 @@
 const ProductCategory = require("../../models/product-category.model");
 const systemConfig = require("../../config/system");
+const createTreeHelper = require("../../helpers/createTree");
 
 // [GET] /admin/products-category
 module.exports.index = async (req, res) => {
   let find = {
     deleted: false,
   };
+
   const record = await ProductCategory.find(find);
+
+  const newRecords = createTreeHelper.createTree(record);
 
   res.render("admin/pages/products-category/index.pug", {
     pageTitle: "Danh mục sản phẩm",
-    record: record,
+    record: newRecords,
   });
 };
 
 // [GET] /admin/products-category/create
 module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+
+  const records = await ProductCategory.find(find).sort({ position: "desc" });
+
+  const newRecords = createTreeHelper.createTree(records);
+
+  // console.log(newRecords);
+
   res.render("admin/pages/products-category/create.pug", {
     pageTitle: "Tạo danh mục sản phẩm",
+    records: newRecords,
   });
 };
 
