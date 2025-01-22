c73a9052480ca802ad6e4b7ecb25fedd9c8e1c1a
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-01-23 01:07:55 +0700

    feat: edit permission

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -29,3 +29,34 @@ module.exports.createPost = async (req, res) => {
   await record.save();
   res.redirect(`${systemConfig.prefixAdmin}/roles`);
 };
+
+// [GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    let find = {
+      _id: id,
+      deleted: false,
+    };
+    const data = await Role.findOne(find);
+    res.render("admin/pages/roles/edit.pug", {
+      pageTitle: "Sửa nhóm quyền",
+      data: data,
+    });
+  } catch (error) {
+    req.flash("error", "Không tìm thấy sản phẩm với id trên");
+    res.redirect(`${systemConfig.prefixAdmin}/roles`);
+  }
+};
+
+// [PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Role.updateOne({ _id: id }, req.body);
+    req.flash("success", "Cập nhập thành công")
+  } catch (error) {
+    req.flash("error", "Cập nhập thất bại")
+  }
+  res.redirect("back");
+};
