97537d202f1c8f2a2704b25c7cff5a393815b412
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-01-28 01:39:54 +0700

    feat: add authorization middleware

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,15 +1,23 @@
 // ** Model **
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
 
 // ** Config **
 const systemConfig = require("../../config/system");
+const { use } = require("../../routes/admin/dashboard.route");
 
 module.exports.requireAuth = async (req, res, next) => {
-  if (
-    !req.cookies.token ||
-    !(await Account.findOne({ token: req.cookies.token }))
-  ) {
+  const user = await Account.findOne({ token: req.cookies.token }).select(
+    "-password"
+  );
+  if (!req.cookies.token || !user) {
     return res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
   }
+  const role = await Role.findOne({
+    _id: user.role_id,
+  }).select("title permissions");
+  res.locals.user = user;
+  res.locals.role = role;
+
   next();
 };
