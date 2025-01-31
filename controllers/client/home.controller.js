498f006d8176f38ee77031e1ecad5ee8beb609b8
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-02-01 01:11:21 +0700

    feat: display a list of new products

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -11,8 +11,18 @@ module.exports.index = async (req, res) => {
 
   const newProducts = productsHelper.priceNewProducts(productsFeatured);
 
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active",
+  })
+    .sort({ position: "desc" })
+    .limit(6);
+
+  const newProductsNew = productsHelper.priceNewProducts(productsNew);
+
   res.render("client/pages/home/index.pug", {
     pageTitle: "Trang chủ",
     productsFeatured: newProducts,
+    productsNew: newProductsNew,
   });
 };
