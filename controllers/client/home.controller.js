91afb68063487b797c6ad714463a49d2fcc8ca42
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-02-01 00:58:16 +0700

    display a list of featured products

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,8 +1,18 @@
-
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
 
 // [GET] /
 module.exports.index = async (req, res) => {
+  const productsFeatured = await Product.find({
+    featured: 1,
+    deleted: false,
+    status: "active",
+  }).limit(6);
+
+  const newProducts = productsHelper.priceNewProducts(productsFeatured);
+
   res.render("client/pages/home/index.pug", {
     pageTitle: "Trang chủ",
+    productsFeatured: newProducts,
   });
 };
