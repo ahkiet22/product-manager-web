37e41726d37860ff27841e2ad0d9e3b284771830
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-02-06 01:24:09 +0700

    build: create collection users

--- helpers/generate.js
+++ helpers/generate.js
@@ -1,5 +1,5 @@
 module.exports.generateRandomString = (length) => {
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrsstuvwxyz0123456789";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
   for (let i = 0; i < length; i++){
       result += characters.charAt(Math.floor(Math.random() * characters.length));
