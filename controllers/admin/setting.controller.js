c5af0ed65d29d822d5ecbd08d3e5309e3fb46b52
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-02-23 21:47:38 +0700

    feat: finish page setting general

--- controllers/admin/setting.controller.js
+++ controllers/admin/setting.controller.js
@@ -2,8 +2,7 @@ const SettingGeneral = require("../../models/settings-general.model");
 
 // [GET] /admin/setting/general
 module.exports.general = async (req, res) => {
-  const generalSetting = await SettingGeneral.find();
-  console.log(generalSetting);
+  const generalSetting = await SettingGeneral.findOne();
   res.render("admin/pages/settings/general.pug", {
     pageTitle: "Cài đặt chung",
     generalSetting: generalSetting,
@@ -12,7 +11,20 @@ module.exports.general = async (req, res) => {
 
 // [PATCH] /admin/setting/general
 module.exports.generalPatch = async (req, res) => {
-  const settingGeneral = new SettingGeneral(req.body);
-  await settingGeneral.save();
-  res.redirect("back");
+  try {
+    let generalSetting = await SettingGeneral.findOne({});
+
+    if (generalSetting) {
+      await generalSetting.updateOne(req.body);
+      req.flash("success", "Cập nhật thành công!");
+    } else {
+      generalSetting = new SettingGeneral(req.body);
+      await generalSetting.save();
+    }
+    res.redirect("/admin/settings/general");
+  } catch (error) {
+    console.error(error);
+    req.flash("error", "Có lỗi xảy ra!");
+    res.redirect("back");
+  }
 };
