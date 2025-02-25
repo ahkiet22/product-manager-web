ba9e7563caff67ec8f07cdd502b5401fa0f4c822
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-02-26 01:46:22 +0700

    feat: display data realtime

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -4,16 +4,33 @@ const User = require("../../models/user.model");
 // [GET] /chat
 module.exports.index = async (req, res) => {
   const userId = res.locals.user.id;
+  const fullName = res.locals.user.fullName;
 
   // SocketIO
   _io.on("connection", (socket) => {
+    socket.sentMydata = false;
     socket.on("CLIENT_SEND_MESSAGE", async (content) => {
-      // Lưu vào database
-      const chat = new Chat({
-        user_id: userId,
-        content: content,
-      });
-      await chat.save();
+      try {
+        if (!socket.sentMydata) {
+          // Lưu vào database
+          const chat = new Chat({
+            user_id: userId,
+            content: content,
+          });
+          await chat.save();
+
+          _io.emit("SERVER_RETURN_MESSAGE", {
+            userId: userId,
+            fullName: fullName,
+            content: content,
+          });
+
+          // Đánh dấu đã gửi tin nhắn
+          socket.sentMydata = true;
+        }
+      } catch (error) {
+        console.error("Error saving message: ", error);
+      }
     });
   });
   // End SocketIO
@@ -22,10 +39,12 @@ module.exports.index = async (req, res) => {
   const chats = await Chat.find({
     deleted: false,
   });
+
   for (const chat of chats) {
     const infoUser = await User.findOne({
       _id: chat.user_id,
     }).select("fullName");
+    // console.log(infoUser);
     chat.infoUser = infoUser;
   }
 
--- public/js/chat.js
+++ public/js/chat.js
@@ -11,3 +11,39 @@ if (formSendData) {
   });
 }
 // End CLIENT_SEND_MESSAGE
+
+// SERVER_RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+  const myId = document.querySelector("[my-id]").getAttribute("my-id");
+  const chatMessages = document.querySelector(".chat .chat-messages");
+
+  const div = document.createElement("div");
+  let htmlFullName = "";
+
+  if (myId == data.userId) {
+    div.classList.add("sent");
+  } else {
+    htmlFullName = `<div class"inner-name">${data.fullName}</div>`;
+    div.classList.add("received");
+  }
+  div.innerHTML = `
+    ${htmlFullName}
+    <div class="message-wrapper">
+      <div class="message-content">
+        <div class="message-bubble">${data.content}</div>
+        <div class="message-time">10:30 AM</div>
+      </div>
+    </div>
+  `;
+
+  chatMessages.appendChild(div);
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+// SERVER_RETURN_MESSAGE
+
+// Scroll Chat to bottom
+const bodyChatMessages = document.querySelector(".chat .chat-messages");
+if (bodyChatMessages) {
+  bodyChatMessages.scrollTop = bodyChatMessages.scrollHeight;
+}
+// End Scroll Chat to bottom
